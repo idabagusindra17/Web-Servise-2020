ae91ad12b6008abe3dc9916e7d7cc25153fbe52a
idabagusindra17 <idabagusindra17@gmail.com>
2020-03-26 16:15:25 +0700

    message

---
+++ appcordova/www/db.php
@@ -0,0 +1,4 @@
+<?php
+ header("Access-Control-Allow-Origin: *");
+ $con = mysqli_connect("localhost","id12789031_localhost","123123","id12789031_akademik") or die ("could not connect database");
+?>
